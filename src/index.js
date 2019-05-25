ede8c30bae288d1e95edbbec80a653207127b201
Daniel Duany <dduany919@gmail.com>
2019-05-24 23:24:30 -0400

    Set up geolocation to determine user position

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  );
+
   return <div>Hi there!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// Geolocation API will get the current position of the user
+// Documentation can be found through Mozilla (MDN)
